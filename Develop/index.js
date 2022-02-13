3ecb64fbedc5f464bbdefcefeac16ed50cbe21b8
Jean-Tae Francis <jeantae95@gmail.com>
2022-02-13 16:09:57 -0500

    removed a period from the readme name

--- Develop/index.js
+++ Develop/index.js
@@ -73,7 +73,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
     .then (function(data){
-        writeToFile(".README.md", generateMarkdown(data))
+        writeToFile("README.md", generateMarkdown(data))
 
     })
 }
