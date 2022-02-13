c6a01d3f94bfabf670ea708a5eefcc05282383c2
Jean-Tae Francis <jeantae95@gmail.com>
2022-02-13 16:08:33 -0500

    literally did the entire project but tons of notes are added to the file itslef

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require("inquirer");
+const Choices = require('inquirer/lib/objects/choices');
+const util = require('util')
+
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'Enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a brief description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'walkthrough',
+        message: 'Please enter a walkthrough of the instillation process and/or deployment links included'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please give us a walkthrough on how to use this application'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose the licenses inlcuded in this project to add it',
+        choices: ['MIT License', 'GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please enter Contributer information'
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: 'Add any third-party assests used as well as any technology used'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Add tests for application and how to use it'
+    },
+    {
+        type: 'input',
+        name: 'question',
+        message: 'Provide name of your GitHub so users can reach out with their questions.',
+    },
+
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Ayy you did it! It generated dude!!!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (function(data){
+        writeToFile(".README.md", generateMarkdown(data))
+
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,116 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  let chosenLicense = `${data.license}`;
+  let readLicense = '';
+  
+  if (chosenLicense === 'MIT License') {
+    showLicense = 'MIT License';
+    badgeLicense = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    readLicense = 'https://opensource.org/licenses/MIT';
+  };
+  if (chosenLicense === 'GNU AGPLv3') {
+    showLicense = 'GNU AGPLv3 License' ;
+    badgeLicense = '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)';
+    readLicense = 'https://www.gnu.org/licenses/agpl-3.0';
+  };
+  if (chosenLicense === 'Mozilla Public License 2.0') {
+    showLicense = 'Mozilla Public License 2.0';
+    badgeLicense = '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)';
+    readLicense = 'https://opensource.org/licenses/MPL-2.0';
+  };
+  if (chosenLicense === 'Apache License 2.0') {
+    showLicense = 'Apache License 2.0';
+    badgeLicense = '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+    readLicense = 'https://opensource.org/licenses/Apache-2.0';
+  };
+  if (chosenLicense === 'Boost Software License 1.0') {
+    showLicense = 'Boost Software License 1.0';
+    badgeLicense = '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+    readLicense = 'https://www.boost.org/LICENSE_1_0.txt';
+  };
+  if (chosenLicense === 'The Unlicense') {
+    showLicense = 'The Unlicense';
+    badgeLicense = '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+    readLicense = 'http://unlicense.org/';
+  };
+
+return `
+# ${data.title}
+
+<br/>
+
+## Project Description
+
+${data.description}
+
+<br/>
+
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributors](#Contributors)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+
+## Installation
+
+${data.walkthrough}
+
+<br/>
+
+## Usage
+
+${data.usage}
+
+<br/>
+
+## Credits
+
+Third-party assets:
+${data.credit}
+
+<br/>
+
+## License
+${badgeLicense}
+
+This application uses the ${showLicense}
+
+For more information, please visit: ${readLicense}
+
+<br/>
+
+## Contributors
+
+Project powered by the contributions of:
+${data.contributors}
+
+## Tests
+
+${data.test}
+
+## Questions
+  If you have any questions, please reach out to a contributor via GitHub:
+  [${data.question}](https://github.com/${data.userName})
+
 
 `;
 }
